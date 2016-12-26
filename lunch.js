c3a67a795e36c99ab5bc4ad07f41e0e318de3283
sukhbinder singh <sukhbindersingh@sukh.home>
2016-12-26 15:47:53 +0000

    added empty functianality

--- lunch.js
+++ lunch.js
@@ -7,11 +7,17 @@ function lunchController($scope){
 	$scope.message="";
 	//$scope.text="";
 	$scope.checkHowMany = function (){
+	
+	if ($scope.textlist ===""){
+		$scope.message= "Please enter data first";
+		return;
+	}
 	var nums=$scope.textlist.split(",").length;
 	//console.log(nums);
 	if (nums >=4){
 		$scope.message= "Too much!";
-	}else{
+	}
+	else{
 		$scope.message="Enjoy!";
 	}
 		
